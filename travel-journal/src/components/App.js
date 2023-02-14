e2c5d8fb71d5124331e769e8c2a140555e1dbc39
Andrew Penny <103720353+A-Penny@users.noreply.github.com>
2023-02-13 22:40:59 -0600

    imported data and used as props

--- travel-journal/src/components/App.js
+++ travel-journal/src/components/App.js
@@ -1,11 +1,27 @@
 import Navbar from "./Navbar";
 import Location from "./Location";
+import data from '../data';
+
 
 function App() {
+  const items = data.map(item => {
+    return <Location 
+        key={item.id}
+        title={item.title}
+        location={item.location}
+        mapLink={item.mapLink}
+        startDate={item.startDate}
+        endDate={item.endDate}
+        description={item.description}
+        imgUrl={item.imgUrl}
+
+    />
+  })
+
   return (
     <div className="App">
       <Navbar />
-      <Location />
+      {items}
     </div>
   );
 }
--- travel-journal/src/components/Location.js
+++ travel-journal/src/components/Location.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { MdLocationOn } from 'react-icons/md';
 
-export default function Location() {
+
+
+export default function Location(props) {
     return (
         <div className='location-container'>
-            <h1>I am the location component</h1>
+            <img src={`${props.imgUrl}`} alt={`${props.title}`}/>
+            
+            <section className='location-info'>
+                <div className='location-top'>
+                    <MdLocationOn className='location-dot'/>
+                    <span className='location-location'>{props.location}</span>
+                    <span className='google-map-link'><a href={`${props.mapLink}`} >View on Google Maps</a></span>
+                </div>
+                <h1 className='location-site'>{props.title}</h1>
+                <h6 className='location-date'>{props.startDate} - {props.endDate}</h6>
+                <p className='location-paragraph'>{props.description}</p>
+            </section>
+            
+
+
         </div>
     )
 }
